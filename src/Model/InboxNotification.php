ad4fc66e842450c510afc9100c517348b4e1c163
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2019-08-03 18:32:59 -0500

    Fixed sender name

--- src/Model/InboxNotification.php
+++ src/Model/InboxNotification.php
@@ -30,9 +30,8 @@ class InboxNotification
         $this->message = $message;
 
         if (!$isSingleton) {
-            $fromEmail = $this->config()->email_from ? $this->config()->email_from : Email::config()->admin_email;
-
-            $this->setFrom([$fromEmail => _t('Inbox.EMAIL_SENDER', 'Inbox Notification')]);
+            $this->setFrom($this->config()->email_from ? $this->config()->email_from : Email::config()->admin_email, 
+                    _t('Inbox.EMAIL_SENDER', 'Inbox Notification'));
             $this->setTo($message->Receiver()->Email);
             $this->setSubject(_t('Inbox.EMAIL_SUBJECT', '[New Message] {title}', array(
                 'title' => $message->Title
